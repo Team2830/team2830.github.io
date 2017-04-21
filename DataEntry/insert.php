1e21907a21020a9c036edef8a1f328158288d4c4
Robotiger <Robotiger@DESKTOP-RVH7PK3>
2017-04-20 19:07:07 -0500

    data entry changes

--- DataEntry/insert.php
+++ DataEntry/insert.php
@@ -1,23 +1,41 @@
 <?php
-
 $team=$_POST["team"];
 $match=$_POST["match"];
-$autoAttemptGear=$_POST["auto-attempt-gear"];
-$autoPlacedGear=$_POST["auto-placed-gear"];
+$autoAttemptGear=(empty($_POST["auto-attempt-gear"])) ? 'no':$_POST["auto-attempt-gear"];
+$autoPlacedGear=(empty($_POST["auto-placed-gear"])) ? 'no':$_POST["auto-placed-gear"];
+$ballPickup=(empty($_POST["ball-pickup"])) ? 'no':$_POST["ball-pickup"];
+$gearPickup=(empty($_POST["gear-pickup"])) ? 'no':$_POST["gear-pickup"];
+$autoPosition = $_POST["pos"];
+$mobility=(empty($_POST["mobility"])) ? 'no':$_POST["mobility"];
+$autokPa=(empty($_POST["auto-kPa"])) ? 0:$_POST["auto-kPa"];
+$autoAccuracy=(empty($_POST["auto-accuracy"])) ? 0:$_POST["auto-accuracy"];
+$teleGearsMade=(empty($_POST["gears-made"])) ? 0:$_POST["gears-made"];
+$teleGearsAttempted=(empty($_POST["gears-attempted"])) ? 0:$_POST["gears-attempted"];
+$telekPa=(empty($_POST["balls-scored"])) ? 0:$_POST["balls-scored"];
+$teleAccuracy=(empty($_POST["teleop-accuracy"])) ? 0:$_POST["teleop-accuracy"];
+$climbAttempt=(empty($_POST["climb-attempt"])) ? 'no':$_POST["climb-attempt"];
+$climbSuccess=(empty($_POST["climb-success"])) ? 'no':$_POST["climb-success"];
+$fouls=(empty($_POST["fouls"])) ? 'none':$_POST["fouls"];
+$yellowCard=(empty($_POST["yellow-card"])) ? 'none':$_POST["yellow-card"];
+$redCard=(empty($_POST["red-card"])) ? 'none':$_POST["red-card"];
+$comments=(empty($_POST["comments"])) ? '':$_POST["comments"];
 
 
-
-if(!empty($team) || !empty($match) ){
-echo "<div>Team: $team- Match:: $match  Entered</div>";
-
-$cvsData = $team . "," . $match . "," . $autoAttemptGear . "," . $autoPlacedGear . "\n"; // Add newline
-
-$fp = fopen("matchData.csv","a"); // $fp is now the file pointer to file $filename
-
+if(!empty($team) || !empty($match) || !empty($autoPosition))
+{
+    echo "<div>Team: $team- Match:: $match $autoPosition Entered</div>";
+    $cvsData = $team . "," . $match . "," . $autoAttemptGear . "," . $autoPlacedGear . "," . $ballPickup . "," . $gearPickup . "," . $autoPosition . "," . $mobility . "," . $autokPa . "," . $autoAccuracy . "," . $teleGearsMade . "," . $teleGearsAttempted . "," . $telekPa . "," . $teleAccuracy . "," . $climbAttempt . "," . $climbSuccess . "," . $fouls . "," . $yellowCard . "," . $redCard . "," . $comments .  "\n";
+    // = "Team,Match,Auto Gear Attempted,Auto Gear Placed,Ball Pickup,Gear Pickup,Auto Position Left,Auto Position Right,Auto Position Center,Mobility,Auto kPa, Auto Accuracy,Teleop Gears Made,Teleop Gears Attempted,Teleop kPa,Teleop Accuracy,Climb Attempt,Climb Success,Fouls,Yellow Card,Red Card,Comments\n";
+    $fp = fopen("matchData.csv", "a");
+   // $fileText  = file_get_contents($fp);
     if($fp)
     {
-        fwrite($fp,$cvsData); // Write information to the file
-        fclose($fp); // Close the file
+  //      $fileText = file_get_contents($fp);
+ //       if(strpos($fileText, $headers) !== false)
+        {
+            fwrite($fp, $cvsData);
+            fclose($fp);
+        }
     }
 }
 include '2017-data.html';
